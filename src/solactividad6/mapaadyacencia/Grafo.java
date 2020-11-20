cec808c8fc05ae7dcf708931424db853e47fbf67
Oscar <oscar.pontebaquero@gmail.com>
2020-11-20 13:09:23 +0100

    Con los métodos que faltaban

--- src/solactividad6/mapaadyacencia/Grafo.java
+++ src/solactividad6/mapaadyacencia/Grafo.java
@@ -13,7 +13,24 @@ import java.util.Iterator;
  */
 public interface Grafo<E,T> {
     public boolean esVacio();
-    public boolean estaArco(Arco<E,T> a);
-    public Iterator<Vertice<E>> adyacentes (Vertice<E> v);
+
+    public boolean estaVertice(Vertice<E> v);
+
+    public boolean estaArco(Arco<E, T> a);
+
+    public Iterator<Vertice<E>> vertices();
+
+    public Iterator<Arco<E, T>> arcos();
+
+    public Iterator<Vertice<E>> adyacentes(Vertice<E> v);
+
+    public void insertarVertice(Vertice<E> v);
+
+    public void insertarArco(Arco<E, T> a);
+
+    public void eliminarVertice(Vertice<E> v);
+
+    public void eliminarArco(Arco<E, T> a);
+
     
 }
--- src/solactividad6/mapaadyacencia/MapaAdyacencia.java
+++ src/solactividad6/mapaadyacencia/MapaAdyacencia.java
@@ -26,13 +26,29 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
         numVertices = 0;
     }
 
-    
     @Override
     public boolean esVacio() {
         if (numVertices == 0) {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public boolean estaVertice(Vertice<E> v) {
+        if (esVacio()) {
+            return false;
+        } else {
+            VerticeConMap<E, T> aux;
+            Iterator<VerticeConMap<E, T>> itr = listVertices.iterator();
+            while (itr.hasNext()) {
+                aux = itr.next();
+                if (aux.getVertice().equals(v)) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
     @Override
@@ -46,7 +62,6 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
                 aux = itr.next();
 
                 if ((aux.getVertice().equals(a.getOrigen()))) {
-//                    Map<Vertice<E>,T> aux2 = aux.getAdy();
                     if (aux.getAdy().get(a.getDestino())!=null && aux.getAdy().get(a.getDestino()).equals(a.getEtiqueta())) {
                         return true;
                     }
@@ -55,6 +70,32 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
             }
             return false;
         }
+    }
+
+    @Override
+    public Iterator<Vertice<E>> vertices() {
+        List<Vertice<E>> vertices = new LinkedList<>();
+        Iterator<VerticeConMap<E, T>> itr = listVertices.iterator();
+        while (itr.hasNext()) {
+            vertices.add(itr.next().getVertice());
+        }
+        return vertices.iterator();
+    }
+
+    @Override
+    public Iterator<Arco<E, T>> arcos() {
+        Set<Arco<E, T>> toret = new HashSet<>();
+        for (VerticeConMap<E, T> vm : listVertices) {
+            Vertice<E> inicio = vm.getVertice();
+            Map<Vertice<E>, T> adyacentes = vm.getAdy();
+            Iterator<Vertice<E>> it = adyacentes.keySet().iterator();
+            while (it.hasNext()) {
+                Vertice<E> fin = it.next();
+                T etiqueta = adyacentes.get(fin);
+                toret.add(new Arco<E, T>(inicio, fin, etiqueta));
+            }
+        }
+        return toret.iterator();
     }
 
     @Override
@@ -70,6 +111,76 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
             }
         }
         return toret;
+    }
+
+    @Override
+    public void insertarVertice(Vertice<E> v
+    ) {
+        if (!estaVertice(v)) {
+            listVertices.add(new VerticeConMap<E, T>(v));
+            numVertices++;
+        } else {
+            System.err.println("Ya existe");
+        }
+    }
+
+    @Override
+    public void insertarArco(Arco<E, T> a
+    ) {
+        if (!estaArco(a)) {
+            if (!estaVertice(a.getOrigen())) {
+                insertarVertice(a.getOrigen());
+            }
+            if (!estaVertice(a.getDestino())) {
+                insertarVertice(a.getDestino());
+            }
+            Iterator<VerticeConMap<E, T>> itr = listVertices.iterator();
+            while (itr.hasNext()) {
+                VerticeConMap<E, T> aux = itr.next();
+                if (a.getOrigen() == aux.getVertice()) {
+                    Map<Vertice<E>, T> map = aux.getAdy();
+                    map.put(a.getDestino(), a.getEtiqueta());
+                }
+            }
+        } else {
+            System.err.println("Error");
+        }
+
+    }
+
+    @Override
+    public void eliminarVertice(Vertice<E> v) {
+        Iterator<VerticeConMap<E, T>> itr = listVertices.iterator();
+        if (estaVertice(v)) {
+            while (itr.hasNext()) {
+                if (itr.next().equals(v)) {
+                    itr.remove();
+                    numVertices--;
+                } else {
+                    Map<Vertice<E>, T> m = itr.next().getAdy();
+                    m.remove(v);
+                }
+            }
+        } else {
+            System.err.println("No se encuentra");
+        }
+    }
+
+    @Override
+    public void eliminarArco(Arco<E, T> a) {
+        Iterator<VerticeConMap<E, T>> itr = listVertices.iterator();
+        if (estaArco(a)) {
+            while (itr.hasNext()) {
+                if (itr.next().getVertice().equals(a.getOrigen())) {
+                    Map<Vertice<E>, T> m = itr.next().getAdy();
+                    m.remove(a.getDestino());
+                    numVertices--;
+                }
+            }
+        } else {
+            System.err.println("No se encuentra");
+        }
+
     }
 
 }
