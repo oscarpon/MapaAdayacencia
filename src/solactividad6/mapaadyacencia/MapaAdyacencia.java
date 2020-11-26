1cdf24135be3bdeb406cc149153f3ed98715e02b
oscarpon <oscar.pontebaquero@gmail.com>
2020-11-26 19:58:24 +0100

    Con comentarios

--- src/solactividad6/mapaadyacencia/MapaAdyacencia.java
+++ src/solactividad6/mapaadyacencia/MapaAdyacencia.java
@@ -18,14 +18,18 @@ import java.util.Set;
  */
 public class MapaAdyacencia<E,T> implements Grafo<E,T> {
   
+  //Cremos una lista de vértices y la variable para saber el número de vértices.
     private List<VerticeConMap<E, T>> listVertices;
     public int numVertices;
 
+//Constructor, donde inicializamos la lista vacía y el numero a 0
     public MapaAdyacencia() {
         listVertices = new LinkedList<>();
         numVertices = 0;
     }
 
+/**Devuelve TRUE si la lista está vacia y FALSE en caso
+contrario */
     @Override
     public boolean esVacio() {
         if (numVertices == 0) {
@@ -34,16 +38,19 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
         return false;
     }
 
+/**Devuelve TRUE si está el vértice que se le pasa como parámetro */
     @Override
     public boolean estaVertice(Vertice<E> v) {
+        //Si está vacio retornamos false porque no está.
         if (esVacio()) {
             return false;
         } else {
+            //Creamos un vértice auxiliar para poder recorrer.
             VerticeConMap<E, T> aux;
             Iterator<VerticeConMap<E, T>> itr = listVertices.iterator();
             while (itr.hasNext()) {
                 aux = itr.next();
-                if (aux.getVertice().equals(v)) {
+                if (aux.getVertice().equals(v)) { //Si el vertice de aux es = a v erntonces devuelve true.
                     return true;
                 }
             }
@@ -51,6 +58,7 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
         }
     }
 
+/**Devuelve TRUE si está el arco y false en caso contrario */
     @Override
     public boolean estaArco(Arco<E, T> a) {
         if (esVacio()) {
@@ -59,9 +67,10 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
             VerticeConMap<E, T> aux;
             Iterator<VerticeConMap<E, T>> itr = listVertices.iterator();
             while (itr.hasNext()) {
-                aux = itr.next();
-
+                aux = itr.next(); //La creamos para recorrer el arco.
+                //Comprobamosm que el vértice es igual al origen del arco a.
                 if ((aux.getVertice().equals(a.getOrigen()))) {
+                    //Si tiene un destino y además el destino del adyacente es igual al arco retorna true.
                     if (aux.getAdy().get(a.getDestino())!=null && aux.getAdy().get(a.getDestino()).equals(a.getEtiqueta())) {
                         return true;
                     }
@@ -72,6 +81,7 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
         }
     }
 
+/**Devuelve un iterator de vértices */
     @Override
     public Iterator<Vertice<E>> vertices() {
         List<Vertice<E>> vertices = new LinkedList<>();
@@ -82,6 +92,7 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
         return vertices.iterator();
     }
 
+/**Devuelve un iterator de arcos */
     @Override
     public Iterator<Arco<E, T>> arcos() {
         Set<Arco<E, T>> toret = new HashSet<>();
@@ -98,6 +109,7 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
         return toret.iterator();
     }
 
+/**Devuelve un iterator de adyacentes */
     @Override
     public Iterator<Vertice<E>> adyacentes(Vertice<E> v
     ) {
@@ -113,6 +125,7 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
         return toret;
     }
 
+/**Inserta un vértice, si ya está ese vértice no deja insertar */
     @Override
     public void insertarVertice(Vertice<E> v
     ) {
@@ -124,18 +137,22 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
         }
     }
 
+/**Inserta un arco. Si el arco ya existe devuelve error */
     @Override
     public void insertarArco(Arco<E, T> a
     ) {
         if (!estaArco(a)) {
+            //Si no existe el origen, insertamos el origen del arco
             if (!estaVertice(a.getOrigen())) {
                 insertarVertice(a.getOrigen());
             }
+            //Si no existe el destino entonces insertamos el destino.
             if (!estaVertice(a.getDestino())) {
                 insertarVertice(a.getDestino());
             }
             Iterator<VerticeConMap<E, T>> itr = listVertices.iterator();
             while (itr.hasNext()) {
+                //Recorremos el iterator y una vez coincidan le asociamos una etiqueta.
                 VerticeConMap<E, T> aux = itr.next();
                 if (a.getOrigen() == aux.getVertice()) {
                     Map<Vertice<E>, T> map = aux.getAdy();
@@ -148,6 +165,7 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
 
     }
 
+/**Elimina un vértice si existe. */
     @Override
     public void eliminarVertice(Vertice<E> v) {
         Iterator<VerticeConMap<E, T>> itr = listVertices.iterator();
@@ -166,14 +184,15 @@ public class MapaAdyacencia<E,T> implements Grafo<E,T> {
         }
     }
 
+/**Elimina un arco si existe. */
     @Override
     public void eliminarArco(Arco<E, T> a) {
         Iterator<VerticeConMap<E, T>> itr = listVertices.iterator();
-        if (estaArco(a)) {
+        if (estaArco(a)) { //Primero comprobamos si está
             while (itr.hasNext()) {
-                if (itr.next().getVertice().equals(a.getOrigen())) {
+                if (itr.next().getVertice().equals(a.getOrigen())) {//Una vez lo localizamos
                     Map<Vertice<E>, T> m = itr.next().getAdy();
-                    m.remove(a.getDestino());
+                    m.remove(a.getDestino());//Eliminamos
                     numVertices--;
                 }
             }
